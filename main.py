f87f459052ef6306906fb7955984034a5eca3f83
Mike Oleshchuk <144498628+Tawxyn@users.noreply.github.com>
2025-04-28 22:38:21 -0700

    Update

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+import asyncio
 import whisper
 import sounddevice as sd
 import scipy.io.wavfile as wav
 
-print(sd.query_devices())
 # Vars
 sample_rate = 44100  
-chunk_duration = 5  
+chunk_duration = 5
+device_index = 2
 
-while (True):
-    myrecording = sd.rec(int(chunk_duration * sample_rate), samplerate=sample_rate, channels=1)
-    sd.wait()
+def record_audio():
+    print("Recording audio...")
+    return sd.rec(int(chunk_duration * sample_rate), samplerate=sample_rate, channels=1, device=device_index)
+
+async def write_audio(myrecording):
+    print("Saving audio to file...")
     wav.write("output_audio.wav", sample_rate, myrecording)
+    
+async def transcribe():
+    print("Transcribing audio...")
     model = whisper.load_model("tiny")
-    result = model.transcribe("audio.mp3")
+    result = model.transcribe("output_audio.wav")
     print(result["text"])
+
+async def main():
+    while True:
+        
+        myrecording = await asyncio.to_thread(record_audio)
+        
+        await write_audio(myrecording)
+        
+        await transcribe()
+        
+        await asyncio.sleep(1)
+        
+asyncio.run(main())
