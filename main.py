8f83c49483f10a1e02551f2f20cde1561af52efc
Tawxyn <oleshchukmike@gmail.com>
2025-05-06 20:57:46 -0700

    Update

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import asyncio
+import os
 import signal
 import time
+import glob
 import warnings
 import logging
 import whisper
 import sounddevice as sd
 import scipy.io.wavfile as wav
 
+# Current directory 
+current_directory = os.getcwd()
 # Remove Terminal User warning
 warnings.filterwarnings('ignore', category=UserWarning)
 #logging 
@@ -34,7 +38,12 @@ def record_block():
 
 async def record_loop():
     while True:
-        await record_audio()
+        try:
+            await record_audio()
+        except:
+            asyncio.CancelledError
+            logging.info("record_audio recived cancelled error...")
+            break
 
 # ---------- WRITE ----------
 async def write_audio():
@@ -42,30 +51,51 @@ async def write_audio():
     filename = f"output_audio{int(time.time())}.wav"
     wav.write(filename, sample_rate, recording)
     await transcribe_queue.put(filename)
-    logging.info("Saved and queued {filename} ...")
 
 async def write_loop():
     while True:
-        await write_audio()
+        try:
+            await write_audio()
+        except:
+            asyncio.CancelledError
+            logging.info("write_audio recived cancelled error...")
+            break
 
 # ---------- TRANSCRIBE ----------
 async def transcribe():
     filename = await transcribe_queue.get()
-    logging.info("Transcribing {filename} ... ")
     result = model.transcribe(filename)
     print(result["text"])
 
 async def transcribe_loop():
     while True:
-        await transcribe()
+        try:
+            await transcribe()
+        except:
+            asyncio.CancelledError
+            logging.info("transcribe_loop recived cancelled error...")
+            break
 
+def delete_wav_files(current_directory):
+    wav_files = glob.glob(os.path.join(current_directory, "*.wav"))
+    i = 0
+    for file_path in wav_files:
+        try:
+            os.remove(file_path)
+            i += 1
+        except Exception as e:
+            print(f"Error deleting {file_path}: {e}")
+    logging.info(f"Deleted: {i} .wav files")
 
 async def shutdown(signal, loop):
     logging.info(f"Recieved exit signal {signal.name} ... ")
     tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
-
+    
+    # Cancel all tasks
     [task.cancel() for task in tasks]
     logging.info(f"Cancelling {len(tasks)} outstanding tasks")
+
+    # Wait for tasks to be all cancled
     await asyncio.gather(*tasks, return_exceptions=True)
     logging.info("Flushing metrics")
     loop.stop()
@@ -75,13 +105,17 @@ async def main():
 
     for s in (signal.SIGHUP, signal.SIGTERM, signal.SIGINT):
         loop.add_signal_handler(s, lambda s=s: asyncio.create_task(shutdown(s, loop)))
-   
-    record_task = asyncio.create_task(record_loop())
-    write_task = asyncio.create_task(write_loop())
-    transcribe_task = asyncio.create_task(transcribe_loop())
-
-    await asyncio.gather(record_task, write_task, transcribe_task)
     
+    try:
+        record_task = asyncio.create_task(record_loop())
+        write_task = asyncio.create_task(write_loop())
+        transcribe_task = asyncio.create_task(transcribe_loop())
+        await asyncio.gather(record_task, write_task, transcribe_task)
+    except:
+        asyncio.CancelledError
+        logging.info("✅ asyncio tasks successfully interupted")
+
+    delete_wav_files(current_directory)
     logging.info("Successfully shutdown hermes services")
 
 if __name__ == "__main__":
